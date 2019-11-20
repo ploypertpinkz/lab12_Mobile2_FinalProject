f754dd6392ea51ef90031d78a74ddee497b1adbc
ploypertpinkz <za_magic_keen@hotmail.com>
2019-11-21 02:49:47 +0700

    Edit Page v.2

--- app/src/main/java/buu/informatics/s59160587/finalcashproject/EditIncomeFragment.kt
+++ app/src/main/java/buu/informatics/s59160587/finalcashproject/EditIncomeFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import buu.informatics.s59160587.finalcashproject.databinding.FragmentEditIncomeBinding
 import timber.log.Timber
 
@@ -22,7 +24,14 @@ class EditIncomeFragment : Fragment() {
         // Inflate the layout for this fragment
         Timber.i("Edit Income Page Called")
         val binding = DataBindingUtil.inflate<FragmentEditIncomeBinding>(inflater,R.layout.fragment_edit_income,container,false)
-
+        binding.editIncomeButton.setOnClickListener { view ->
+            Timber.i("editIncomeButton Called")
+            Toast.makeText(context, "Edit Income", Toast.LENGTH_SHORT).show()
+        }
+        binding.deleteIncomeButton.setOnClickListener { view ->
+            Timber.i("deleteIncomeButton Called")
+            Toast.makeText(context, "Delete Income", Toast.LENGTH_SHORT).show()
+        }
 
         return binding.root
     }
--- app/src/main/java/buu/informatics/s59160587/finalcashproject/EditOutcomeFragment.kt
+++ app/src/main/java/buu/informatics/s59160587/finalcashproject/EditOutcomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import buu.informatics.s59160587.finalcashproject.databinding.FragmentEditOutcomeBinding
 import timber.log.Timber
@@ -22,7 +23,14 @@ class EditOutcomeFragment : Fragment() {
         // Inflate the layout for this fragment
         Timber.i("Edit Outcome Page Called")
         val binding = DataBindingUtil.inflate<FragmentEditOutcomeBinding>(inflater,R.layout.fragment_edit_outcome,container,false)
-
+        binding.editOutcomeButton.setOnClickListener { view ->
+            Timber.i("editOutcomeButton Called")
+            Toast.makeText(context, "Edit Outcome", Toast.LENGTH_SHORT).show()
+        }
+        binding.deleteOutcomeButton.setOnClickListener { view ->
+            Timber.i("deleteOutcomeButton Called")
+            Toast.makeText(context, "Delete Outcome", Toast.LENGTH_SHORT).show()
+        }
 
         return binding.root
     }
