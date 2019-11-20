38e5bd5b25e1f11e34e0b149a55a145939fbfc74
ploypertpinkz <za_magic_keen@hotmail.com>
2019-11-21 02:25:55 +0700

    Add action Main page

--- app/src/main/java/buu/informatics/s59160587/finalcashproject/MainBankFragment.kt
+++ app/src/main/java/buu/informatics/s59160587/finalcashproject/MainBankFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import buu.informatics.s59160587.finalcashproject.databinding.FragmentMainBankBinding
 import timber.log.Timber
 
@@ -20,8 +22,22 @@ class MainBankFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = DataBindingUtil.inflate<FragmentMainBankBinding>(inflater,R.layout.fragment_main_bank,container,false)
         Timber.i("Main Page Called")
+
+        val binding = DataBindingUtil.inflate<FragmentMainBankBinding>(inflater,R.layout.fragment_main_bank,container,false)
+        binding.incomeButton.setOnClickListener { view ->
+            Timber.i("income Button Called")
+            Toast.makeText(context, "gotoOpenBank", Toast.LENGTH_SHORT).show().run {
+                view.findNavController().navigate(R.id.action_mainBankFragment_to_saveIncomeFragment)
+            }
+        }
+        binding.outcomeButton.setOnClickListener { view ->
+            Timber.i("outcome Button Called")
+            Toast.makeText(context, "gotoOpenBank", Toast.LENGTH_SHORT).show().run {
+                view.findNavController().navigate(R.id.action_mainBankFragment_to_saveOutcomeFragment)
+            }
+        }
+
         return binding.root
     }
 
--- app/src/main/java/buu/informatics/s59160587/finalcashproject/OpenBankFragment.kt
+++ app/src/main/java/buu/informatics/s59160587/finalcashproject/OpenBankFragment.kt
@@ -25,7 +25,7 @@ class OpenBankFragment : Fragment() {
         binding.openBankButton.setOnClickListener { view ->
             Timber.i("Open Bank Button Called")
             Toast.makeText(context, "gotoOpenBank", Toast.LENGTH_SHORT).show().run {
-                view.findNavController().navigate(R.id.action_loginBankFragment_to_openBankFragment)
+                view.findNavController().navigate(R.id.action_openBankFragment_to_mainBankFragment)
             }
         }
         setHasOptionsMenu(true)
